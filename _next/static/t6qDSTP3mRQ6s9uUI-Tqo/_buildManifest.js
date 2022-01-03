b981c4cb77bfc445263a4447ef4b5588ac72c5ac
adriansyah <adrian.syah@healthengine.com.au>
2022-01-03 08:06:06 +0000

    Deploying to gh-pages from  @ a52b6bd1edee58831abcd739f570b4b3e901a90e 🚀

---
+++ _next/static/t6qDSTP3mRQ6s9uUI-Tqo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/270-535b53888a016951.js","static/chunks/pages/index-776a116e8cc32abb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
