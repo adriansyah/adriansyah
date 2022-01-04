4b46ab8e097fb02d830e98393bb8d2554868e51d
Adrian Syah <adriansyahk@gmail.com>
2022-01-04 23:11:16 +0800

    Add worker

--- pages/showcases/algorithms/palindrome-check.tsx
+++ pages/showcases/algorithms/palindrome-check.tsx
@@ -4,6 +4,7 @@ import { Navigation } from "../../../src/common/components/Navigation/Navigation
 import { CodeEditor } from "../../../src/common/components/CodeEditor/CodeEditor";
 import { ChangeEvent, useState } from "react";
 import { XCircleIcon, CheckCircleIcon } from "@heroicons/react/outline";
+import Spinner from "../../../public/spinner.svg";
 
 const IsPalindrome: NextPage = () => {
   const [code, setCode] = useState(
@@ -13,14 +14,50 @@ const IsPalindrome: NextPage = () => {
   const [error, setError] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
   const [isSubmitted, setSubmitted] = useState(false);
+  const [isRunning, setRunning] = useState(false);
+
+  const executeWorker = () => {
+    const codeToExceute = `${code} postMessage(isPalindrome('${checkData}'))`;
+    let timerId: NodeJS.Timeout;
+
+    const blob = new Blob([codeToExceute], {
+      type: "text/javascript",
+    });
+
+    const blobUrl = URL.createObjectURL(blob);
+    const worker = new Worker(blobUrl);
+
+    worker.addEventListener("message", (e) => {
+      setIsCorrect(e.data);
+      setSubmitted(true);
+      clearTimeout(timerId);
+      setRunning(false);
+    });
+
+    // add a listener for errors from the Worker
+    worker.addEventListener("error", (e) => {
+      const string = e.message.toString();
+      setError(string);
+      setSubmitted(false);
+      clearTimeout(timerId);
+      setRunning(false);
+    });
+
+    timerId = setTimeout(() => {
+      worker.terminate();
+      setSubmitted(false);
+      setError("Function is taking longer than 5 second");
+      setRunning(false);
+    }, 5000);
+  };
 
   const checkPalindrome = () => {
     setSubmitted(false);
     if (!checkData) return;
+    setRunning(true);
     setError("");
     try {
-      setIsCorrect(eval(`${code} isPalindrome('${checkData}')`));
-      setSubmitted(true);
+      executeWorker();
     } catch (er: unknown) {
       if (er instanceof Error) {
         setError(er.message);
@@ -122,9 +159,13 @@ const IsPalindrome: NextPage = () => {
             <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
               <button
                 type="submit"
+                disabled={isRunning}
                 onClick={checkPalindrome}
                 className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
+                {isRunning && (
+                  <Spinner className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" />
+                )}
                 Submit
               </button>
             </div>
