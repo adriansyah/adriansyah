b096f7921671111e9017ddc25a8de2de31b0bda8
Adriansyah <adriansyahk@gmail.com>
2024-01-08 23:33:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const removeImports = require("next-remove-imports")();
 
 module.exports = removeImports({
   reactStrictMode: true,
-   i18n: {
-    locales: ['en-AU'],
-    defaultLocale: 'en-AU',
-  },
   images: {
     domains: ["source.unsplash.com"],
     loader: "custom",
